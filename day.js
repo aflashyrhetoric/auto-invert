ec2387681c78db2d4e8c6d800f0b1772a00784e7
Kevin Oh <aflashyrhetoric@gmail.com>
2018-06-09 14:47:32 -0400

    Turns all text white

--- day.js
+++ day.js
@@ -1,11 +1,11 @@
-"use strict";
+// "use strict";
 
-// Removes the stylesheet element
-document.getElementById('nightMessenger')
-.parentNode
-.removeChild(document.getElementById('nightMessenger'));
+// // Removes the stylesheet element
+// document.getElementById('nightMessenger')
+// .parentNode
+// .removeChild(document.getElementById('nightMessenger'));
 
-// Turn off sync preference
-chrome.storage.sync.set({ activated : false  }, function() {
-  console.log("Deactivated."); 
-});
+// // Turn off sync preference
+// chrome.storage.sync.set({ activated : false  }, function() {
+//   console.log("Deactivated."); 
+// });
--- night.js
+++ night.js
@@ -1,4 +1,41 @@
 "use strict";
 
-console.log('activated');
-document.querySelector('body').style.backgroundColor = 'black';
+console.log('activated')
+
+let BLACK = '#252324'
+let WHITE = '#fff';
+
+let TEXT_ELEMENTS = [
+  'h1',
+  'h2',
+  'h3',
+  'h4',
+  'h5',
+  'h6',
+  'p',
+  'a',
+  'span',
+  'code pre',
+  'li'
+]
+
+let textElements = []
+
+// Get all elements
+TEXT_ELEMENTS.forEach((textEl) => {
+  textElements.push(document.querySelectorAll(textEl))
+})
+
+// Filter out null/empty elements
+textElements = textElements.filter(el => {
+  return el.length !== 0;
+})
+
+let flattenedTextElements = []
+
+textElements.forEach(el => {
+  el.forEach(el => flattenedTextElements.push(el))
+})
+
+flattenedTextElements.forEach(el => el.style.color = WHITE);
+
